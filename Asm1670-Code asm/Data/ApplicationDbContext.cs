95dd7b3ef928442687f9f267de7adbac2c0ad209
hieugch18038 <hieunhgch18038@fpt.edu.vn>
2022-08-22 21:41:05 +0700

    Upload

---
+++ Asm1670-Code asm/Data/ApplicationDbContext.cs
@@ -0,0 +1,138 @@
+﻿using Asm1670.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asm1670.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Request> Request { get; set; }
+        public DbSet<Author> Author { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedCategory(builder);
+            SeedBook(builder);
+            SeedAuthor(builder);
+            SeedUser(builder);
+            SeedRole(builder);
+            SeedUserRole(builder);
+        }
+        private void SeedUserRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "1",
+                    RoleId = "1"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "2",
+                    RoleId = "2"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "3",
+                    RoleId = "3"
+                }
+            );
+        }
+        private void SeedRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "Admin"
+                },
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "Customer",
+                    NormalizedName = "Customer"
+                },
+                new IdentityRole
+                {
+                    Id ="3",
+                    Name = "Store Owner",
+                    NormalizedName = "Store Owner"
+                }
+            );
+        }
+        private void SeedUser(ModelBuilder builder)
+        {
+            //tạo tài khoản test cho admin & customer
+            var admin = new IdentityUser
+            {
+                Id = "1",
+                Email = "admin@gmail.com",
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "admin@gmail.com"
+            };
+            var customer = new IdentityUser
+            {
+                Id = "2",
+                Email = "customer@gmail.com",
+                UserName = "customer@gmail.com",
+                NormalizedUserName = "customer@gmail.com"
+            };
+            var storeowner = new IdentityUser
+            {
+                Id = "3",
+                Email = "storeowner@gmail.com",
+                UserName = "storeowner@gmail.com",
+                NormalizedUserName = "storeowner@gmail.com"
+            };
+
+            //khai báo thư viện để mã hóa mật khẩu cho user
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            //set mật khẩu đã mã hóa cho từng user
+            admin.PasswordHash = hasher.HashPassword(admin, "123456");
+            customer.PasswordHash = hasher.HashPassword(customer, "123456");
+            storeowner.PasswordHash = hasher.HashPassword(storeowner, "123456");
+
+            //add 2 tài khoản test vào bảng User
+            builder.Entity<IdentityUser>().HasData(admin, customer, storeowner);
+        }
+        
+        private void SeedBook(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Name = "The War of Jenkins' Ear", Description= "abcd", Image= "https://img.thriftbooks.com/api/images/i/l/A8355E53B30072A52D98408A600E40C47BF77518.jpg", Quantity = 10, Price = 13000, CategoryId = 2, AuthorId = 1 },
+                new Book { Id = 2, Name = "African Kaiser: General Paul Von Lettow-Vorbeck and the Great War in Africa, 1914-1918", Description = "abcd", Image = "https://img.thriftbooks.com/api/images/m/1b3c2d90b93acea06d9807e06ad6ff03a47a1076.jpg", Quantity = 10, Price = 13000, CategoryId = 2, AuthorId = 2 },
+                new Book { Id = 3, Name = "Diary of a Wimpy Kid", Description = "abcd", Image = "https://img.thriftbooks.com/api/images/m/81690cdfa2eaab727c29d81b308efba1dec70a55.jpg", Quantity = 10, Price = 13000, CategoryId = 2, AuthorId = 3 }
+            );
+        }
+        private void SeedCategory(ModelBuilder builder)
+        {
+            builder.Entity<Category>().HasData(
+                new Category { Id = 1, CategoryName = "History" },
+                new Category { Id = 2, CategoryName = "Entertainment" },
+                new Category { Id = 3, CategoryName = "Commic" }
+                );
+        }
+        private void SeedAuthor(ModelBuilder builder)
+        {
+            builder.Entity<Author>().HasData(
+                new Author { Id = 1, AuthorName = "Robert Gaudi" },
+                new Author { Id = 2, AuthorName = "Jeff Kinney" },
+                new Author { Id = 3, AuthorName = "Masashi Kishimoto" }
+                );
+        }
+    }
+}
---
+++ Asm1670-Code asm/Migrations/20220822081506_new.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Asm1670.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(maxLength: 128, nullable: false),
+                    ProviderKey = table.Column<string>(maxLength: 128, nullable: false),
+                    ProviderDisplayName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    RoleId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    LoginProvider = table.Column<string>(maxLength: 128, nullable: false),
+                    Name = table.Column<string>(maxLength: 128, nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Image = table.Column<string>(nullable: false),
+                    Price = table.Column<float>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    AuthorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Book_Author_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Author",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Book_category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    OrderQuantity = table.Column<int>(nullable: false),
+                    OrderPrice = table.Column<double>(nullable: false),
+                    OrderDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cart_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1", "306a7e28-ed85-4177-a858-9f2d1e7413cb", "Admin", "Admin" },
+                    { "2", "66b88b06-d2a8-495a-973b-b30b6568c3a7", "Customer", "Customer" },
+                    { "3", "a9404f30-db95-4d3d-95a9-86eaf777c55c", "Store Owner", "Store Owner" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "1", 0, "dde09998-7dda-4cd5-b581-590c9f4601c0", "admin@gmail.com", false, false, null, null, "admin@gmail.com", "AQAAAAEAACcQAAAAEOkeozjlB/R01qpc/XFtzx26Zq+UlSUDNWyNBMtkwzRJntfD8mguBQ18GzjJ/Wvdxg==", null, false, "0405b54b-4b3b-4958-9afe-169b8df14215", false, "admin@gmail.com" },
+                    { "2", 0, "40f1cc30-76fc-49a9-9399-46e529a27d8c", "customer@gmail.com", false, false, null, null, "customer@gmail.com", "AQAAAAEAACcQAAAAEK6LgWQJT79p7MuEs3Hp7bk8aO5oop3zHGQkgnyxk+ETKzicqa0vrtgtfUuqgR2DBw==", null, false, "8fd0f531-d7d6-4140-943b-12ace90a99df", false, "customer@gmail.com" },
+                    { "3", 0, "4dbdf6c5-0502-4d3a-a407-13ad74acab87", "storeowner@gmail.com", false, false, null, null, "storeowner@gmail.com", "AQAAAAEAACcQAAAAEBS2oDKV7EHPCVOaKN48LlUglLDreW1IT2udPiwGZl+A5gP4vWHI6LWoI0TmPWsEyg==", null, false, "3fba5e2b-9e27-467c-9e3f-92fa145e7ab0", false, "storeowner@gmail.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Author",
+                columns: new[] { "Id", "AuthorName" },
+                values: new object[,]
+                {
+                    { 1, "Robert Gaudi" },
+                    { 2, "Jeff Kinney" },
+                    { 3, "Masashi Kishimoto" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "category",
+                columns: new[] { "Id", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "History" },
+                    { 2, "Entertainment" },
+                    { 3, "Commic" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "UserId", "RoleId" },
+                values: new object[,]
+                {
+                    { "1", "1" },
+                    { "2", "2" },
+                    { "3", "3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "AuthorId", "CategoryId", "Description", "Image", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 2, "abcd", "https://img.thriftbooks.com/api/images/i/l/A8355E53B30072A52D98408A600E40C47BF77518.jpg", "The War of Jenkins' Ear", 13000f, 10 },
+                    { 2, 2, 2, "abcd", "https://img.thriftbooks.com/api/images/m/1b3c2d90b93acea06d9807e06ad6ff03a47a1076.jpg", "African Kaiser: General Paul Von Lettow-Vorbeck and the Great War in Africa, 1914-1918", 13000f, 10 },
+                    { 3, 3, 2, "abcd", "https://img.thriftbooks.com/api/images/m/81690cdfa2eaab727c29d81b308efba1dec70a55.jpg", "Diary of a Wimpy Kid", 13000f, 10 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorId",
+                table: "Book",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_CategoryId",
+                table: "Book",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_BookId",
+                table: "Cart",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Cart");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "category");
+        }
+    }
+}
